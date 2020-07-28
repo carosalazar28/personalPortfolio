ae865bc46333668c3e5c0467e6eb6d7cf0603ac5
Carolina <cindy.s.r.28@hotmail.com>
2020-07-27 20:28:38 -0500

    add effects in button burger for read nav

--- js/effects.js
+++ js/effects.js
@@ -1,1 +1,24 @@
-console.log('Hola')
+const ipad = window.matchMedia("screen and (max-width: 765px)")
+const menu = document.querySelector(".menuNav")
+const burgerButton = document.querySelector("#burger-menu")
+
+ipad.addListener(validation)
+
+function validation(event) {
+    if (event.matches) {
+        burgerButton.addEventListener("click", hideShow)
+    } else {
+        burgerButton.removeEventListener("click", hideShow)
+    }
+    console.log(Event.matches)
+}
+
+validation(ipad)
+
+function hideShow() {
+    if (menu.classList.contains("is-active")) {
+        menu.classList.remove("is-active")
+    } else {
+        menu.classList.add("is-active")
+    }
+}
